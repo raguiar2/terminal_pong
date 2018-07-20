fd36d3102affa2c17024f8cae2b39add647324d2
raguiar2 <raguiar2@stanford.edu>
2018-07-19 22:05:48 -0700

    added paddle functionality

--- terminal_pong.c
+++ terminal_pong.c
@@ -2,7 +2,8 @@
 #include <stdlib.h>
 #include <unistd.h>
 #define EXIT_SUCCESS 0
-
+#define PADDLE "|\n |\n |\n |"
+#define BALL "o"
 // Stores our balls position
 typedef struct _Ball {
     int x;
@@ -13,29 +14,36 @@ typedef struct _Direction {
     int x;
     int y;
 } Direction;
+// store paddle position
+typedef struct _Paddle {
+    int x;
+    int y;
+} Paddle;
 
-int main() {
-  initscr();              // Start ncurses
-  nodelay(stdscr, TRUE);  // Don't wait for \n for getch to parse input
-  cbreak();               // Switch off input buffering
-  curs_set(FALSE);        // Don't place a cursor on the screen
-  int height = 0;         // Window height
-  int width = 0;          // Window width
+// Takes in a user input and moves the paddle that direction
+Paddle moveUserPaddle(Paddle userPaddle){
+  if (getch() == '\033') { // if the first value is esc
+    getch(); // skip the [
+    switch(getch()) { // the real value
+        case 'A':
+            // code for arrow up
+	  userPaddle.y--;
+	  break;
+        case 'B':
+            // code for arrow down
+	  userPaddle.y++;
+            break;
+    }
+  }
+  mvprintw(userPaddle.y,userPaddle.x,PADDLE);
+  return userPaddle;
+}
 
-// initialize our ball struct and direction struct
-Ball b = {
-    1,1
-};
-Direction d = {
-    1,1
-};
-
-// Get terminal window dimensions (rows and characters)
-getmaxyx(stdscr, height, width);
-
- while (getch() == ERR) {
+void playGame(Ball b, Direction d, Paddle userPaddle, Paddle computerPaddle, int width, int height) {
+   while (true) {
+    userPaddle = moveUserPaddle(userPaddle);
     // print ball
-    mvprintw(b.y,b.x,"o");
+    mvprintw(b.y,b.x,BALL);
 
     // move ball for next frame
     b.y += d.y;
@@ -57,4 +65,36 @@ getmaxyx(stdscr, height, width);
     usleep(50000);  // Sleep to show output (Single frame)
     clear();        // Clear output
 }
+}
+
+int main() {
+  initscr();              // Start ncurses
+  nodelay(stdscr, TRUE);  // Don't wait for \n for getch to parse input
+  cbreak();               // Switch off input buffering
+  curs_set(FALSE);        // Don't place a cursor on the screen
+  int height = 0;         // Window height
+  int width = 0;          // Window width
+
+// Get terminal window dimensions (rows and characters)
+getmaxyx(stdscr, height, width);
+  
+// initialize our ball struct and direction struct
+Ball b = {
+    width/2,height/2
+};
+Direction d = {
+    1,1
+};
+ 
+// initalze the two paddles
+Paddle userPaddle = {
+  1, height/2
+};
+
+Paddle computerPaddle = {
+  width-1, width/2 
+};
+ 
+ playGame(b,d,userPaddle,computerPaddle, width, height);
+
 }
